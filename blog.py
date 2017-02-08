5800b0fce44e7b7cc1b469a76161dbe04b756529
winterinferno <dabeckham@ucdavis.edu>
2017-02-08 10:51:39 -0800

    Added documentation to blog.py

--- blog.py
+++ blog.py
@@ -8,18 +8,21 @@ from flask import Flask, request, render_template
 
 from wtforms import Form, TextField, TextAreaField, SubmitField, validators
 
-class MainForm(Form):
-  data = None
-
+#Creates basic html form with two TextField boxes for entry
 class PostForm(Form):
   title=TextField('Title', [validators.Length(min=1)])
   body=TextField('Body', [validators.Length(min=1)])
 
+#Creates flask object to run app
 app = Flask(__name__)
 
+#Returns connection to blog database
 def connectDB():
   return sqlite3.connect('blog.db')
 
+#Connects to database. Counts the total number of entries in the database + 1 to find out the post_id of the next blog post.
+#Takes two parameters: title of article, and body of article
+#Parameters and post_id are inserted into the database with a sql statement
 def insertDB(title,body):
   try:
     conn = connectDB()
@@ -32,6 +35,7 @@ def insertDB(title,body):
   except:
     return False
 
+#prints the column headers within a row of db
 def printDBInfo():
   conn = connectDB()
   c = conn.cursor()
@@ -39,12 +43,14 @@ def printDBInfo():
     print(row)
   conn.close()
 
+#Deletes everything from the database
 def clearDB():
   conn = connectDB()
   conn.execute('DELETE FROM posts')
   conn.commit()
   conn.close()
 
+#Prints everything in database
 def printDB():
   conn = connectDB()
   c = conn.cursor()
@@ -52,6 +58,7 @@ def printDB():
     print(row)
   conn.close()
 
+#Creates endpoint at /posts and handles a GET request to the blog database and returns all entries in posts
 @app.route('/posts', methods=['GET'])
 def posts():
   conn = connectDB()
@@ -67,10 +74,13 @@ def posts():
 
   return json.dumps({'posts': formattedPosts}), 200, {'Content-Type': 'application/json'}
 
+#Creates endpoint at the root which returns the api homepage
+#From here, users can choose between adding a new post or displaying all posts
 @app.route('/', methods=['GET'])
 def index():
   return render_template("index.html")
 
+#Creates endpoint at /post which invokes the PostForm object. If we POST, then we select the data entered into the title and body fields and pass them into our insertDB function and return to the root page
 @app.route('/post', methods=['GET','POST'])
 def post():
   form = PostForm(request.form)
